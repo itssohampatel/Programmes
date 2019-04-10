0f83035fa8d9df7647c0b415929c70f10347e8bd
mastikhor333 <40313162+mastikhor333@users.noreply.github.com>
2019-04-10 13:49:36 +0530

    php

--- PHP/databasebconnection.php
+++ PHP/databasebconnection.php
@@ -3,7 +3,7 @@ $name=$_POST('uname');
 $pass=$_POST('psw');
 echo 'your first value is:'.$name."<br/>";
 echo 'your second value is:'.$psw."<br/>";
-$db=mysqli_connect("localhost","sonu","22165810","db");
+$db=mysqli_connect("localhost","username","password","database");
 $query="insert into name values('".$name"','".$psw."')";
 if(mysqli_query($db,$query)){
 	echo "submitted";
