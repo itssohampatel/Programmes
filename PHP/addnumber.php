63fa2b0ec9b05bc5547dc2a199bfa3318a4ea755
Mr.333 <40313162+mastikhor333@users.noreply.github.com>
2020-04-06 14:42:55 +0530

    Create addnumber.php

---
+++ PHP/addnumber.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Number</title>
+	<style type="text/css">
+		h1{
+			font-weight: 400;
+			font-size: 60px;
+			text-align: center;
+			margin-top: 40vh;
+		}
+		span{
+			background-color:rgba(255,0,0,0.5);
+			padding: 10px;
+
+		}
+	</style>
+</head>
+<body>
+	<?php
+		$num1 = 20;
+		$num2 = 50;
+	    $sum=$num1+$num2;
+	?>
+	<h1>Your Sum is <?php echo "<span>$sum</span>"?></h1>
+</body>
+</html
